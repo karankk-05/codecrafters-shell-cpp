b2f824681f901a531c873fc8220208d99c7234b7
Karan Keer <karank23@iitk.ac.in>
2026-05-10 19:19:52 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+  while(1){
   std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
   std::cout << input << ": command not found" << std::endl;}
+  }
